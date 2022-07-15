cf66049ac56e96f03dae59e08c970b25c55db815
Daniel Kolomeetz <kolomeetz.dany@yandex.ru>
2022-07-16 00:03:49 +0300

    Save cursor last position

--- lua/user/autocmd.lua
+++ lua/user/autocmd.lua
@@ -8,10 +8,23 @@
 -- })
 
 -- Change go imports order when starting to write the whole buffer to a file
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  pattern = { "*.go" },
-  callback = function()
-    require("user.lsp.handlers").goimports(1000)
-  end,
+local goimports = function ()
+      require("user.lsp.handlers").goimports(1000)
+end
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = goimports
 })
 
+-- Save cursor last position
+-- from :help last-position-jump
+local savePosition = function ()
+  vim.cmd [[
+    autocmd FileType <buffer> ++once
+      \ if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif
+  ]]
+end
+vim.api.nvim_create_autocmd("BufRead",{
+  pattern = "*",
+  callback = savePosition
+})
