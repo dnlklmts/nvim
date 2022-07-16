6074d9831b50fdb64bebdeff57b475b60b4c5b17
Daniel Kolomeetz <kolomeetz.dany@yandex.ru>
2022-07-16 21:45:11 +0300

    Hook auto formatting on BufWritePre event

--- lua/user/autocmd.lua
+++ lua/user/autocmd.lua
@@ -1,41 +1,41 @@
 local cmp_ok, cmp = pcall(require, "cmp")
 if not cmp_ok then
-  vim.notify("failed to load cmp plugin")
-  return
+	vim.notify("failed to load cmp plugin")
+	return
 end
 
 -- Change go imports order when starting to write the whole buffer to a file
-local goimports = function (wait_ms)
-  local params = vim.lsp.util.make_range_params()
-  params.context = {only = {"source.organizeImports"}}
-  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
-  for _, res in pairs(result or {}) do
-    for _, r in pairs(res.result or {}) do
-      if r.edit then
-        vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
-      else
-        vim.lsp.buf.execute_command(r.command)
-      end
-    end
-  end
+local goimports = function(wait_ms)
+	local params = vim.lsp.util.make_range_params()
+	params.context = { only = { "source.organizeImports" } }
+	local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+	for _, res in pairs(result or {}) do
+		for _, r in pairs(res.result or {}) do
+			if r.edit then
+				vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
+			else
+				vim.lsp.buf.execute_command(r.command)
+			end
+		end
+	end
 end
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = goimports
+	pattern = "*.go",
+	callback = goimports,
 })
 
 -- Save cursor last position
 -- from :help last-position-jump
-local savePosition = function ()
-  vim.cmd [[
+local savePosition = function()
+	vim.cmd([[
     autocmd FileType <buffer> ++once
       \ if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif
-  ]]
+  ]])
 end
-vim.api.nvim_create_autocmd("BufRead",{
-  pattern = "*",
-  callback = savePosition
+vim.api.nvim_create_autocmd("BufRead", {
+	pattern = "*",
+	callback = savePosition,
 })
 
 --[[
@@ -63,10 +63,19 @@ vim.api.nvim_create_autocmd( {"TextChangedI", "TextChangedP"}, {
 --]]
 
 -- https://vi.stackexchange.com/a/1985
-local rmFormatOpts = function ()
-  vim.cmd [[ set fo-=c fo-=r fo-=o ]]
+local rmFormatOpts = function()
+	vim.cmd([[ set fo-=c fo-=r fo-=o ]])
 end
 vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = "*",
-  callback = rmFormatOpts
+	pattern = "*",
+	callback = rmFormatOpts,
+})
+
+-- Code formatting after saving file
+local formatAfterSave = function()
+	vim.lsp.buf.formatting_sync({ async = true })
+end
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = formatAfterSave,
 })
